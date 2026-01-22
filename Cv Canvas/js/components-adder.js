e096eade7788fe05ff606695977a317a7da443ae
neojihun00-lgtm <neojihun00@gmail.com>
2026-01-22 18:11:48 +0500

    second commit

--- Cv Canvas/js/components-adder.js
+++ Cv Canvas/js/components-adder.js
@@ -6,5 +6,5 @@ function component_adder(id, content){
         .then(data => component.innerHTML = data)
 }
 
-component_adder('header', '/Cv Canva/components/header.html');
-component_adder('footer', '/Cv Canva/components/footer.html');
+component_adder('header', '/Cv Canvas/components/header.html');
+component_adder('footer', '/Cv Canvas/components/footer.html');
